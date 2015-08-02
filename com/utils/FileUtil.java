038e5fb9f031a22106e157bb49410c0a9ef52284
Tim Shi <wutian713@sina.cn>
2015-08-02 13:15:06 +0800

    add write method in FileUtil.java

--- com/utils/FileUtil.java
+++ com/utils/FileUtil.java
@@ -2,4 +2,6 @@ public class FileUtil{
 public void readFile(String filePath){
  System.out.println("Reading file..");
 }
+public void writeFile(String filePath){
+ System.out.println("Writing file...");
 }
